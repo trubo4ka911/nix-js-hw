45a7045c7fb983432b9d0a27b844a74c354b1580
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2023-01-17 18:36:55 +0200

    task

--- main.js
+++ main.js
@@ -133,3 +133,137 @@
 // ----------------------------Завдання No 9. Отримати середній вік
 // Напишіть функцію getAverageAge(users), яка приймає масив об'єктів із властивістю age
 // та повертає середній вік.
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 29 };
+// let arr = [vasya, petya, masha];
+
+// const getAverageAge = (users) => {
+//   return users.reduce((total, user) => total + user.age, 0) / users.length;
+// };
+// console.log(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
+
+// Завдання No 10. Залишити унікальні елементи масиву
+// Нехай arr – масив рядків.
+// Напишіть функцію unique(arr), яка повертає масив, що містить лише унікальні елементи arr.
+// Наприклад:
+
+// const unique = (arr) => {
+//     return arr.filter((el, index, array) => array.indexOf(el) === index);
+// };
+// let strings = [
+//   "кришна",
+//   "кришна",
+//   "харе",
+//   "харе",
+//   "харе",
+//   "харе",
+//   "кришна",
+//   " кришна ",
+//   ":-O",
+// ];
+// console.log(unique(strings)); // кришна, харе, :-O
+
+// ----------------------------Завдання No 11. Фільтрування унікальних елементів масиву
+// Допустимо, у нас є масив arr.
+// Створіть функцію unique(arr), яка поверне масив унікальних значень масиву arr, що не повторюються.
+// Наприклад:
+// function unique(arr) {
+//     return Array.from(new Set(arr))
+// }
+
+// або через spread
+// function unique(arr) {
+//   return [...new Set(arr)];
+// }
+// let values = [
+//   "Hare",
+//   "Krishna",
+//   "Hare",
+//   "Krishna",
+//   "Krishna",
+//   "Krishna",
+//   "Hare",
+//   "Hare",
+//   ":-O",
+// ];
+// console.log(unique(values)); // Hare,Krishna,:-O
+// // PS Тут ми використовуємо рядки, але значення може бути будь-якого типу.
+// // PPS Використовуйте Set для збереження унікальних значень.
+
+// ------------------------------Завдання No 12. Чи незалежні лічильники?
+// Тут ми робимо два лічильники: counter і counter2, використовуючи ту саму функцію makeCounter.
+// Вони незалежні? Що покаже другий лічильник? 0,1 чи 2,3 чи щось ще?
+
+// Буде також //0,1 так як в counter різне лексичне оточення і тому різні свої ссилки на функцію, до якої вони ссилаються
+
+// function makeCounter() {
+//   let count = 0;
+//   return function () {
+//     return count++;
+//   };
+// }
+// let counter = makeCounter();
+// let counter2 = makeCounter();
+// console.log(counter()); // 0
+// console.log(counter()); // 1
+// console.log(counter2()); //? 0
+// console.log(counter2()); //? 1
+
+// ------------------------------Завдання No 13. Об'єкт лічильника
+// Тут об'єкт лічильника створено за допомогою функції-конструктора.
+// Чи працюватиме він? Що покаже?
+
+// Буде працювати. В обох функціях однакове лексичне оточення. Для кожного виклику make Counter() створюється нове лексичне оточення функції зі своїм власним count
+
+// function Counter() {
+//   let count = 0;
+//   this.up = function () {
+//     return ++count;
+//   };
+//   this.down = function () {
+//     return --count;
+//   };
+// }
+// let counter = new Counter();
+// console.log(counter.up()); //? 1
+// console.log(counter.up()); //? 2
+// console.log(counter.up()); //? 3
+// console.log(counter.up()); //? 4
+// console.log(counter.down()); //? 3
+// console.log(counter.down()); //? 2
+
+// -------------------Завдання No 14. Обчислити факторіал
+// Факторіал натурального числа – це число, помножене на себе мінус один, потім на себе
+// мінус два, і так далі до 1. Факторіал n позначається як n!
+// Визначення факторіалу можна записати як:
+
+// n! = n * (n - 1) * (n - 2) * ... * 1
+
+// Приклади значень для різних n:
+// 1! = 1
+// 2! = 2 * 1 = 2
+// 3! = 3 * 2 * 1 = 6
+// 4! = 4 * 3 * 2 * 1 = 24
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
+// Завдання – написати функцію factorial(n), яка повертає n!, використовуючи рекурсію.
+// alert( factorial(5) ); // 120
+// PS Підказка: n! можна записати як n*(n-1)! Наприклад: 3! = 3 * 2! = 3 * 2 * 1! = 6
+
+// function factorial(n) {
+//   let product = 1;
+//   while (n > 1) {
+//     product *= n;
+//     n--;
+//   }
+//   return product;
+// }
+// або через цикл
+// function factorial(n) {
+//   let i,
+//     product = 1;
+//   for (i = 2; i <= n; i++) product *= i;
+//   return product;
+// }
+// console.log(factorial(5));
